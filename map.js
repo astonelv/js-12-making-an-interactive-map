f951b18501f523772dcf7ec2cd813692ee6e22b2
astonelv <158239756+astonelv@users.noreply.github.com>
2024-04-17 17:25:30 -0700

    grr

--- map.js
+++ map.js
@@ -1,1 +1,106 @@
-const APIKey = "fsq3iwaB6lDc4Ps23y3JjB3F2iBG1bsXOnxWcWWVrL1/c+Y=";   
+let myMap;
+let markers = [];
+
+async function getCoords(){
+    pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+    return [pos.coords.latitude, pos.coords.longitude]
+}
+console.log(getCoords())
+
+async function createMap(){
+    let coords = await getCoords();
+    myMap = L.map('map').setView(coords, 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(myMap);
+
+
+const marker = L.marker(coords).addTo(myMap)
+marker.bindPopup('<p1><b> You are Here. </b></p1>').openPopup()
+}
+createMap()
+
+
+async function search() {
+  let userLocation= await getCoords()
+  let businessType = document.getElementById('select-body')
+
+  businessType.addEventListener('change', async (event) => {
+    markers.forEach(marker => myMap.removeLayer(marker))
+    markers = [];
+
+    let userChoice = event.target.value;
+    try{
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'fsq3ffi/8CyAl0LRJEz8+5TWf4I2wWFGVLhmLBbFxhcXB7E='
+        }
+      };
+     let response = await fetch(`https://api.foursquare.com/v3/places/search?query=${userChoice}&11=${userLocation}&limit=5`, options)
+     let data = await response.json();
+     console.log(data)
+
+    let locations = data.results.map(result =>({
+      name: result.name,
+      latlng: [result.geocodes.main.latitude, result.geocodes.main.longitude],
+    }))
+    console.log(locations)
+
+    locations.forEach(location => {
+      let marker = L.marker(location.latlng).addTo(myMap)
+      marker.bindPopup(`${location.name} `).openPopup();
+      markers.push(marker);async function search() {
+        let userLocation= await getCoords()
+        let businessType = document.getElementById('select-body')
+      
+        businessType.addEventListener('change', async (event) => {
+          markers.forEach(marker => myMap.removeLayer(marker))
+      
+          let userChoice = event.target.value;
+          try{
+            const options = {
+              method: 'GET',
+              headers: {
+                accept: 'application/json',
+                Authorization: 'fsq3ffi/8CyAl0LRJEz8+5TWf4I2wWFGVLhmLBbFxhcXB7E='
+              }
+            };
+           let response = await fetch(`https://api.foursquare.com/v3/places/search?query=${userChoice}&11=${userLocation}&limit=5`, options)
+           let data = await response.json();
+           console.log(data)
+      
+          let locations = data.results.map(result =>({
+            name: result.name,
+            latlng: [result.geocodes.main.latitude, result.geocodes.main.longitude],
+          }))
+          console.log(locations)
+      
+          locations.forEach(location => {
+            let marker = L.marker(location.latlng).addTo(myMap)
+            marker.bindPopup(`${location.name}`).openPopup();
+            markers.push(marker)
+          })
+      
+          }
+          catch(error){
+            console.log("Fetch Error", error)
+          }
+        })
+      }
+      
+      search()
+    })
+
+    }
+    catch(error){
+      console.log("Fetch Error", error)
+    }
+  })
+}
+search()
